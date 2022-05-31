1d0dc8de9d1ded7fcb3f6ab6608b8dc3402a69e4
Uri <uri555459@gmail.com>
2022-05-31 22:14:41 +0300

    Added 2 plugins, cors and morgan

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const cors = require('cors')
+// Выводит логи запросов в консоль
+const morgan = require('morgan')
 
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
@@ -7,8 +10,10 @@ const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
