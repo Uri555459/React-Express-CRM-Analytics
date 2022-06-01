442869b19d1b671fea906b42923913256317503e
Uri <uri555459@gmail.com>
2022-06-01 11:56:37 +0300

    Wrete register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
 module.exports.login = (req, res) => {
   res.status(200).json({
     login: {
@@ -7,8 +10,31 @@ module.exports.login = (req, res) => {
   })
 }
 
-module.exports.register = (req, res) => {
-  res.status(200).json({
-    register: 'from controller',
-  })
+module.exports.register = async (req, res) => {
+  const candidate = await User.findOne({ email: req.body.email })
+
+  if (candidate) {
+    // Пользователь существует, выводиь ошибку
+    res.status(409).json({
+      message: 'Такой email уже занят.',
+    })
+  } else {
+    // Создаем пользователя
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt),
+    })
+
+    try {
+      await user.save()
+      res.status(201).json(user)
+      // res.status(201).json({
+      //   message: 'Пользователь успешно создан',
+      // })
+    } catch (error) {
+      // Обработать ошибку
+    }
+  }
 }
